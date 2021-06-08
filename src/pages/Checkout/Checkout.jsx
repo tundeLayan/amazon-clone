e6a84c13ef6d7dab7aec21eb461eaba2c85b0548
tundeLayan <tunderoxyafolayan@gmail.com>
2021-06-08 17:10:11 +0100

    Checkout page and styling

---
+++ src/pages/Checkout/Checkout.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { CheckoutProduct, Subtotal } from "../../components";
+import "./Checkout.scss";
+import { useStateValue } from "../../StateProvider";
+
+const Checkout = () => {
+  const {
+    state: { basket, user },
+    dispatch
+  } = useStateValue();
+  console.log("Basket from checkout", basket);
+
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+          className="checkout__ad"
+        />
+        <div className="">
+          <h2 className="checkout__title">Your Shopping basket</h2>
+          {basket.map((item, i) => {
+            return (
+              <CheckoutProduct
+                key={i}
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+                dispatch={dispatch}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div className="checkout__right">
+        <Subtotal basket={basket} />
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
