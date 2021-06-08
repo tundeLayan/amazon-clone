7c63909c668003f3fced70d88059cb380b924b24
tundeLayan <tunderoxyafolayan@gmail.com>
2021-06-08 17:12:07 +0100

    Checkout product component

---
+++ src/components/CheckoutProduct/CheckoutProduct.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "./CheckoutProduct.scss";
+import { getRating } from "../../utils";
+
+interface IProps {
+  id: string;
+  image: string;
+  title: string;
+  price: string;
+  rating: number;
+  dispatch?: any;
+  hideButton?: boolean;
+}
+const CheckoutProduct = (props: IProps) => {
+  const { id, image, title, price, rating, dispatch, hideButton } = props;
+  const removeFromBasket = (id: string) => {
+    // dispatch action that will remove item from basket
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id
+    });
+    toast.success("Item removed from basket!");
+  };
+  return (
+    <div className="checkoutProduct">
+      <ToastContainer />
+      <img className="checkoutProduct__image" alt="product" src={image} />
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="checkoutProduct__rating">{getRating(rating)}</div>
+        {!hideButton && (
+          <button onClick={() => removeFromBasket(id)}>
+            Remove from basket
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutProduct;
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Product } from "./Product/Product";
+export { default as Subtotal } from "./Subtotal/Subtotal";
+export { default as CheckoutProduct } from "./CheckoutProduct/CheckoutProduct";
+export { default as Order } from "./Order/Order";
