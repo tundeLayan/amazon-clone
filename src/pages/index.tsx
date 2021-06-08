efaceac815903662585da280a008729c36716659
tundeLayan <tunderoxyafolayan@gmail.com>
2021-06-08 17:10:35 +0100

    Pages bundle default export

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home/Home";
+export { default as Checkout } from "./Checkout/Checkout";
+export { default as Login } from "./Login/Login";
+export { default as Payment } from "./Payment/Payment";
+export { default as Orders } from "./Orders/Orders";
