8566aac65d1e15ba32cb2b7b9fa543d9f12349dc
tundeLayan <tunderoxyafolayan@gmail.com>
2021-06-26 02:48:35 +0100

    Header style update

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
-import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
-import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import MenuIcon from '@material-ui/icons/Menu';
+// import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "react-router-dom";
 
 import { ReactComponent as Logo } from "../Assets/amazonLogo.svg";
@@ -27,11 +28,16 @@ const Header = () => {
         <Link to={routes.home.path}>
           <Logo />
         </Link>
-        <Link to={''} style={{ textDecoration: 'none', marginRight: '10px' }}>
-        <span className="deliverLocation">
-          <LocationOnOutlinedIcon className="location"/>
-          <p><span>Deliver to <br/></span>{location}</p>
-        </span>
+        <Link to={""} style={{ textDecoration: "none", marginRight: "10px" }}>
+          <span className="deliverLocation">
+            <LocationOnOutlinedIcon className="location" />
+            <p>
+              <span>
+                Deliver to <br />
+              </span>
+              {location}
+            </p>
+          </span>
         </Link>
         <div className="header__search">
           <input className="header__searchInput" type="text" />
@@ -62,23 +68,29 @@ const Header = () => {
               <span className="header__optionLineTwo">& Orders</span>
             </div>
           </Link>
-          <div className="header__option">
+          {/* <div className="header__option">
             <span className="header__optionLineOne">Your</span>
             <span className="header__optionLineTwo">Prime</span>
-          </div>
-          <Link to={routes.checkout.path}>
+          </div> */}
+          <Link to={routes.checkout.path} className="header__lastlink">
             <div className="header__optionBasket">
-              <ShoppingBasketIcon />
+              <ShoppingCartOutlinedIcon className="header__basket" />
               <span className="header__optionLineTwo header__basketCount">
                 {basket.length}
               </span>
+              <p className="header__cart">Cart</p>
             </div>
           </Link>
         </div>
       </div>
       <div className="below-header">
         <div className="left">
-          <Link to={""}><span style={{display: 'inline-flex'}}> <MenuIcon className="hamburger-menu"/> All</span></Link>
+          <Link to={""}>
+            <span style={{ display: "inline-flex", alignItems: "center" }}>
+              {" "}
+              <MenuIcon className="hamburger-menu" /> All
+            </span>
+          </Link>
           <Link to={""}>Today's Deals</Link>
           <Link to={""}>Customer Service</Link>
           <Link to={""}>Gift Cards</Link>
